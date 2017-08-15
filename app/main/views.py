b249d85c87d480fa2a06db777531954024aa707c
Maxim <osadchuk.m.01@gmail.com>
2017-08-15 18:54:05 +0300

    Some changes.

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,9 @@
-from flask import render_template
-from flask_login import login_required
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_required, current_user
 
-from ..decorators import interviewed
+from ..decorators import interviewed, not_interviewed
 from . import main
+from .forms import PollForm
 
 
 @main.route('/')
@@ -12,6 +13,15 @@ def home():
     return render_template('index.html')
 
 
-@main.route('/poll')
+@main.route('/poll', methods=['GET', 'POST'])
+@login_required
+@not_interviewed
 def poll():
-    return render_template('poll.html')
+    form = PollForm()
+    if request.method == 'POST':
+        user = current_user._get_current_object()
+        user.update_interest_from_form(form)
+        user.interviewed = True
+        flash('Your interest has been saved.')
+        return redirect(url_for('main.home'))
+    return render_template('poll.html', form=form)
