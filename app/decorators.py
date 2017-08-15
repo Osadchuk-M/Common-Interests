4cf739b6eab0a0c1e6a2496bc033f024054d59de
Maxim <osadchuk.m.01@gmail.com>
2017-08-15 18:51:40 +0300

    add not interviewed decorator

--- app/decorators.py
+++ app/decorators.py
@@ -12,3 +12,13 @@ def interviewed(func):
         flash('You should pass the poll before get access to this page.')
         return redirect(url_for('main.poll'))
     return decorated_view
+
+
+def not_interviewed(func):
+    @wraps(func)
+    def decorated_view(*args, **kwargs):
+        if not current_user.interviewed:
+            return func(*args, **kwargs)
+        flash('You are already interviewed.')
+        return redirect(url_for('main.home'))
+    return decorated_view
